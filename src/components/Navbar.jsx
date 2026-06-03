3cff254111cd6077d9a2cf25e2ec080f4c671870
Wesley Dyron Gimutao <90434934+Sweasley@users.noreply.github.com>
2026-06-03 20:34:17 +0800

    initial push:

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+
+      {/* Left / Brand */}
+      <div className="flex-1">
+        <a className="text-2xl font-bold text-primary hover:text-secondary">
+          W Gimutao
+        </a>
+      </div>
+
+      {/* Mobile dropdown */}
+      <div className="flex-none lg:hidden">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost">
+            <i className="bi bi-list text-2xl"></i>
+          </label>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li><a>About</a></li>
+            <li><a>Experiences</a></li>
+            <li><a>Project</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </div>
+      </div>
+
+      {/* Desktop menu */}
+      <div className="hidden lg:flex flex-none">
+        <ul className="menu menu-horizontal px-1 gap-2">
+          <li><a className="hover:text-primary text-lg">About</a></li>
+          <li><a className="hover:text-primary text-lg">Experiences</a></li>
+          <li><a className="hover:text-primary text-lg">Project</a></li>
+          <li><a className="hover:text-primary text-lg">Contact</a></li>
+        </ul>
+
+       
+      </div>
+
+    </div>
+  )
+}
