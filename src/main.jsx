76307f3343b2cd27e462d510905599d857caa469
Wesley Dyron Gimutao <90434934+Sweasley@users.noreply.github.com>
2026-06-06 21:35:10 +0800

    asds

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
+import { createRoot } from "react-dom/client";
 
-import { createRoot } from 'react-dom/client'
+import App from "./App.jsx";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import App from './App.jsx'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-
-createRoot(document.getElementById('root')).render(
-
-    <App />
-
-)
+createRoot(document.getElementById("root")).render(<App />);
